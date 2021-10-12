9b5130a38d93f4d147e7f1bd1dd026d2c5c67012
max Icolumi <max77648@gmail.com>
2021-10-12 11:40:54 +0300

    Добавил кнопку "New day"(нужно отладить отступ)

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const state = getStoredStateOrDefault({ 
+  const state = getStoredStateOrDefault({
     counter: 40
   })
 
@@ -8,14 +8,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const $gauge = document.querySelector('.gauge')
   setGaugePercent($gauge, state.counter)
-  
-  $incrButton.addEventListener('click', function() {
+
+  $incrButton.addEventListener('click', function () {
     state.counter = Math.min(state.counter + 1, 100)
     saveState(state)
     setGaugePercent($gauge, state.counter)
   })
 
-  $decrButton.addEventListener('click', function() {
+  $decrButton.addEventListener('click', function () {
     state.counter = Math.max(state.counter - 1, 0)
     saveState(state)
     setGaugePercent($gauge, state.counter)
