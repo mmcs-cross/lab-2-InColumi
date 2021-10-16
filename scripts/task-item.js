90aeb347d893ce7e44b1b18c6e405fe5ba973842
Maxim <max77648@gmail.com>
2021-10-16 21:13:04 +0300

    Добавил зачеркивание по нажатию. Начал поле ввода

--- scripts/task-item.js
+++ scripts/task-item.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', (e) => {
-  const $rectagle = document.querySelector(".rectangle")
-  const $icon = $rectagle;
+  const $task_item = document.querySelector(".task-item")
+  const $rectagle = $task_item.querySelector(".rectangle")
+  const $text = $task_item.querySelector(".task-item__text")
+  const $item_remove = $task_item.querySelector(".task-item_remove")
   
-  console.log($icon.getElementsByClassName(''))
   $rectagle.addEventListener('click', (e) => {
     e.currentTarget.classList.toggle('rectangle_checked_true')
+    $text.classList.toggle('task-item_text_line-through')
+    $item_remove.classList.toggle('task-item_remove_view_true')
+  })
+
+  $task_item.addEventListener('click', (e)=> {
+    e.target.classList.toggle('task-item_blue-border')
   })
 })
